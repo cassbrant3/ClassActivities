863ed61cba417abfa22a7ecd56cacf40c7162890
Cassandra Brantley <cassiebrant552@gmail.com>
2023-11-14 21:03:23 -0600

    JS Variables and Primitives Part 2

---
+++ Week2/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,34 @@
+// create a variable called "name" that references a string
+const name = "Cassandra";
+// create a variable called "favoriteSong" that references a string
+let favoriteSong = "Chill Bill";
+// create a variable called "wage" that references a number
+let wage = 23.57;
+// create a variable called "age" that references a number
+let age = 19;
+// create a variable called "onlyChild" that references a boolean
+const onlyChild = false;
+// create a variable called "satisfied" that references a boolean
+let satisfied = false;
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+let favoriteThing = 2;
+console.log(typeof favoriteThing);
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong = "The Way You Make Me Feel";
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = "pets";
+console.log(typeof favoriteThing);
+// Happy Birthday!
+// Reassign "age" to a new value
+age = 20;
+// You got a big raise at work.
+// Reassign "wage" to a new value
+wage = 30.01;
+// Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisified = true;
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
+console.log("Hello, my name is," + {"Cassandra"} + ", my favorite song is" {favoriteSong} + ", and my favorite thing are my" {favoriteThing} );
