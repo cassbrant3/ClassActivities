b634b726f26fc24730e4175bd5fad40723979cc0
Cassandra Brantley <cassiebrant552@gmail.com>
2023-11-14 23:47:23 -0600

    JS Variables and Primitives Part 4

---
+++ Week2/2.2.4-JSMorePrimitives.js
@@ -0,0 +1,27 @@
+// create five variables and assign them values
+let string = "Oog";
+let number = 23;
+let boolean = false;
+let empty = null;
+let variable; 
+
+// each variable should reference a different primitive data type
+// REMEMBER: you should never DELIBERATELY assign "undefined" to a variable
+
+// print the type of each variable in this order:
+// string, number, boolean, undefined, null
+console.log(typeOf string);
+console.log(typeOf number);
+console.log(typeOf boolean);
+console.log(typeOf variable);
+console.log(typeOf empty);
+// create a variable that references a template literal
+// inside the template literal, use two of the above variables
+
+// reassign the value of the variable that references "null"
+
+// print the value and its type
+
+// print a variable that causes a ReferenceError
+
+// alter the previous line to no longer cause a ReferenceError
