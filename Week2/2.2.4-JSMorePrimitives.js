acfa5bfc05181ad561810cb3f73a5b9b4efbcf5f
Cassandra Brantley <cassiebrant552@gmail.com>
2023-11-23 12:44:12 -0600

    Week 2 Class Activities 2.2.4 and 2.3.4

--- Week2/2.2.4-JSMorePrimitives.js
+++ Week2/2.2.4-JSMorePrimitives.js
@@ -10,18 +10,19 @@ let variable;
 
 // print the type of each variable in this order:
 // string, number, boolean, undefined, null
-console.log(typeOf string);
-console.log(typeOf number);
-console.log(typeOf boolean);
-console.log(typeOf variable);
-console.log(typeOf empty);
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof boolean);
+console.log(typeof variable);
+console.log(typeof empty);
 // create a variable that references a template literal
 // inside the template literal, use two of the above variables
-
+let template = `"Oog" is "false" `;
 // reassign the value of the variable that references "null"
-
+variable = "boo";
 // print the value and its type
-
+console.log(variable);
 // print a variable that causes a ReferenceError
-
+console.log(error);
 // alter the previous line to no longer cause a ReferenceError
+let error = "hurray";
